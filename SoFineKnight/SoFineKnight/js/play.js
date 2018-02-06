af176b7f0c90829a4bba57778b1d1b60a01ea455
Stoycho Anastasov Nenov <1062073@ucn.dk>
2018-02-06 12:42:45 +0100

    enemies done

--- SoFineKnight/SoFineKnight/js/play.js
+++ SoFineKnight/SoFineKnight/js/play.js
@@ -7,7 +7,10 @@ var upKey;
 var downKey;
 var rightKey;
 var leftKey;
-
+var platforms;
+var enemies;
+var text;
+var enemyTimer=1;
 var playState = {
     
     create: function() {
@@ -42,7 +45,20 @@ var playState = {
             ground.scale.setTo(1, 1);
             ground.body.immovable = true;
         }
-              
+        //Enemies
+        enemies = game.add.group();
+        game.physics.enable(enemies, Phaser.Physics.ARCADE);
+        enemies.enableBody = true;
+
+        for (var i = 0; i < 5; i++)
+        {
+            var enemy = enemies.create(game.world.randomX, game.world.randomY, 'skeleton',i);
+            enemy.animations.add('move', [0,1,2,3,4,5,6,7,8,9,10,11,12], 12, true);
+            enemy.body.gravity.y = 100;
+
+
+        }
+
         // Create the player sprite and enable physics.
         this.player = game.add.sprite(16, game.world.height - 80, 'player');
         game.physics.enable(this.player, Phaser.Physics.ARCADE);
@@ -51,7 +67,7 @@ var playState = {
         this.player.outOfBoundsKill = true;
 
 
-        text = game.add.text(16, 16, 'unknown', { fontSize: '32px', fill: '#ffffff' })
+        text = game.add.text(16, 16, 'test', {fontSize: '32px', fill: '#ffffff'})
     },
 
     update: function() {
@@ -59,20 +75,37 @@ var playState = {
         // When the player sprite and sprite overlap, the win function
         // is called.
         game.physics.arcade.overlap(this.player, this.win, this.Win, null, this);
-        text.text = this.player.body.y;
+
         if (!this.player.exists) {
             this.lose(this);
         }
-
         //Collision with ground
         var hitPlatform = game.physics.arcade.collide(this.player, platforms);
+        game.physics.arcade.collide(enemies, platforms);
         this.player.body.velocity.x = 0;
         this.player.body.velocity.y = 0;
 
+
+        enemies.forEach(function(enemy) {
+            if(enemyTimer<100&&enemyTimer>0){
+                enemy.body.velocity.x = 100;
+                enemy.scale.x = 1;
+
+            }else if(enemyTimer>100){
+                enemyTimer=-99;
+            }else{
+                enemy.body.velocity.x = -100;
+                enemy.scale.x = -1;
+            }
+            enemy.animations.play('move');
+            if(enemy.body.x+10>playState.player.body.x&&enemy.body.x-10<playState.player.body.x&&enemy.body.y+10>
+                playState.player.body.y&&enemy.body.y-10<playState.player.body.y){
+                playState.lose(playState);
+            }
+        });
+        enemyTimer++;
         // Finally, we give the human player a means to move the sprite.
         // Enabling x-axis movement:
-        this.player.body.velocity.x = 0;
-        this.player.body.velocity.y = 0;
 
         if (leftKey.isDown){
             this.player.body.velocity.x = -400;
